51688ff1a2ec089fa89f2ff77d0a3adf9ad07604
EonJo_Mac <sim04170@gmail.com>
2021-07-30 22:33:55 +0900

    Add : make movies module

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'max-len':[
+    'max-len': [
       {
-        "ignorePattern": "^import |^export {(.*?)}"
+        ignorePattern: '^import |^export {(.*?)}',
       },
     ],
   },
